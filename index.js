dade836be5f7c2e53ec32aea64d28efa8e0bf440
David G Chaves <davidgchaves@gmail.com>
2018-10-26 13:04:26 +0200

    Add `start` and bare bones `onClick`

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+// start :: void -> void
+function start() {
+  document
+    .querySelector(".calculator-container")
+    .addEventListener("click", onClick);
+}
+
+// onClick :: Event -> void
+function onClick(event) {
+  console.log(event.target.innerText);
+}
+
 // printToScreen :: String -> void
 function printToScreen(message) {
   document.querySelector(".result").innerText = message;
 }
+
+// PROGRAM
+start();
