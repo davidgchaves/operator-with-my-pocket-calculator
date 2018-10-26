cafcb111c3cb5830dae88eb1072204f32e1ca16b
David G Chaves <davidgchaves@gmail.com>
2018-10-26 13:12:30 +0200

    Use `switch` in `onClick`

--- index.js
+++ index.js
@@ -7,7 +7,40 @@ function start() {
 
 // onClick :: Event -> void
 function onClick(event) {
-  console.log(event.target.innerText);
+  const char = event.target.innerText;
+  switch (char) {
+    case "0":
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+      console.log(char);
+      break;
+
+    case "+":
+    case "−":
+    case "×":
+    case "÷":
+      console.log(char);
+      break;
+    case "C":
+      console.log(char);
+      break;
+    case "←":
+      console.log(char);
+      break;
+    case "=":
+      console.log(char);
+      break;
+    default:
+      console.error("Something went wrong!");
+      break;
+  }
 }
 
 // printToScreen :: String -> void
